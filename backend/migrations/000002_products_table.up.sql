a0de39b72acf910633f61464ef20cb7ca8f284fb
acool-kaz <pastapappie24@gmail.com>
2023-04-10 05:55:59 +0600

    migrate: new fields for product

--- backend/migrations/000002_products_table.up.sql
+++ backend/migrations/000002_products_table.up.sql
@@ -2,7 +2,9 @@ CREATE TABLE IF NOT EXISTS products(
     id serial primary key,
     user_id integer not null,
     name varchar(255) not null,
+    short_description text not null,
     description text not null,
+    tag text not null,
     price numeric not null,
     foreign key (user_id) references users (id) on delete cascade
 );
